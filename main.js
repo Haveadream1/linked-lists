1eb0ad5209ee2524162285d6f5dca99156593293
Haveadream1 <260602david@gmail.com>
2025-03-28 16:59:29 +0100

    refactor: review tests

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-// A head node is the first node in the list
-// A tail node is the last node in the list
-
-// [ NODE(head) ] -> [ NODE ] -> [ NODE(tail) ] -> null
-
 import LinkedList from "./linkedList.js";
 
 const list = new LinkedList();
 
 list.append("dog");
 list.append("cat");
-// list.prepend("mouse");
+list.append("parrot");
+list.prepend("mouse");
 console.log(list.size());
 
-// console.log(list.head()); // ISSUE
+console.log(list.getHead());
 
 console.log(list.tail());
 console.log(list.at(1));
 
-// list.pop(); // ISSUE
+list.pop();
 
 console.log(list.contains("dog"));
 console.log(list.find("cat"));
-
-
-// list.append("parrot");
-// list.append("hamster");
-// list.append("snake");
-// list.append("turtle");
 
 console.log(list.toString());
